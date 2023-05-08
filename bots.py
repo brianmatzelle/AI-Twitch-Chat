357e6fe3889e8a976bc27fdd62496701c0cc2b7d
brianmatzelle <bmatzel1@binghamton.edu>
2023-05-08 11:39:12 -0400

    fixed max num of responding bots bug

--- bots.py
+++ bots.py
@@ -20,7 +20,11 @@ class Bots:
     # Generate bot responses
     def generate_bot_responses(self, input_text, debug_message):
         # Determine the number of bots to respond
-        num_responding_bots = random.randrange(1, self.config["max_num_of_responding_bots"]+1)
+        if len(self.arr) == 1:
+            num_responding_bots = 1
+        else:
+            num_responding_bots = random.randrange(1, self.config["max_num_of_responding_bots"])
+            
         debug_message.emit(f"Number of bots responding: {num_responding_bots}")
         
         # Randomly select a quarter of the bots
